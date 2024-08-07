ebab4247e8e8e27bf1ef530d391eaab430ea37ab
Felipe Rondello <ferondellodev@gmail.com>
2024-08-06 21:56:42 -0300

    parte do programa final finalizado

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace StoreApp
+﻿
+
+using StoreApp.Entities;
+using System.ComponentModel;
+
+namespace StoreApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthdate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("How many items to this order: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data");
+
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodPrice = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+
+            }
+
         }
     }
 }
